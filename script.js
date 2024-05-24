b1dec6faaa3f934b1657e0226c3cb85b5fdb454a
Your Name <zohaibabbas8557@gmail.com>
2024-05-24 20:46:02 +0500

    startup of javascipt and learn the variable

---
+++ script.js
@@ -0,0 +1,48 @@
+                            // WORKING ON JAVASCIPT//
+
+            /* ALERT :- the popup on the web window when you taking your conformation/giving warning etc.
+            syntax is as following the keyword "alert" then the parenthesis ans and taking the string with the double qoute and the last terminator to end the statement .*/
+            alert("Muhammmad Zohaib Abbas");
+
+            //Window.alert//
+            // a global element//
+            window.alert("Muhammad Zohaib abbas")
+            
+            // the concole.log//
+            /* Work same as the alert but provide the output in the inpect window of the web...*/ 
+            console.log("Muhammad Zohaib Abbas");
+
+            // document:- to write in the html document using the javascipt
+             document.write("Muhammad Zohaib Abbas")
+
+                                 //  VARIABLES //
+    //  a keyword var is used to declare a variable
+    //  syntax :- var variable_name = "value";
+
+    var firstName; // declaring the variable...
+    var lastName = "Abbas"; // initializing the varibale...
+
+// rules for variable:-  ->no use of space   ->no of builtin keywords   ->use of numbers/alphabets/characters ($,_)
+//->use of capital letters but take care of CASE SENSITIVITY ->use of camelCasing ->use of desciptive variable name
+
+    //string variable
+     var firstName = "Muhammad Zohiab ";
+     var lastName = "Abbas";
+     console.log("My name is "+ firstName + lastName + ".");
+
+     //number variables
+     var num1 = 23;
+     var num2 = 45;
+     //here if i assign new value to the variablee declared before like num2=67----its value will change;
+     num2 = 67;
+
+     var total=num1+num2;
+     console.log(total);// the output will be 68 if the num2 has old value and new value ----see the variable assignment..
+    
+
+     //cancatination of string variables 
+
+     var num1 = 34;
+     var num2 = "34";
+     var total = num1 + num2;
+     console.log(total);//output will not be the addition of the nnumber instead it will stick with the number likewise the output will be 3434
