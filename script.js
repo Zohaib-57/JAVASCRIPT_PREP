320813f0b8f47e66a40ed3f494a6d28e5817ba0e
Your Name <zohaibabbas8557@gmail.com>
2024-08-28 12:00:11 +0500

    updated

--- script.js
+++ script.js
@@ -601,26 +601,3 @@ i am commenting these lines*/
 //   let thePara = document.getElementById("slowLoris").innerHTML;
 //   document.getElementById("readMe").innerHTML= thePara
 // }
-
-function readParagraphText() {
-  var paragraph = document.getElementById('myParagraph');
-  var output = document.getElementById('output');
-  var paragraphText = paragraph.textContent;
-  output.textContent = 'Current paragraph text: "' + paragraphText + '"';
-}
-
-// Function to set new paragraph text
-function setParagraphText() {
-  var paragraph = document.getElementById('myParagraph');
-  var newTextInput = document.getElementById('newTextInput');
-  var output = document.getElementById('output');
-  var newText = newTextInput.value;
-
-  if (newText.trim() === '') {
-    output.textContent = 'Please enter some text to set!';
-  } else {
-    paragraph.textContent = newText;
-    output.textContent = 'Paragraph text has been updated!';
-    newTextInput.value = '';
-  }
-}
